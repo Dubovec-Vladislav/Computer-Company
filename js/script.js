f71b10e8eb59cc0f463c931cef3cfd5d384be15e
Vladislav <dubovecvladislav0@gmail.com>
2023-04-27 16:50:21 +0300

    Import slider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const productSlider = new Swiper('.swiper', {
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
